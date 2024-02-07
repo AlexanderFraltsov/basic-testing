bcb24a7bf506bdb201c58b47797b93b4253bfe51
AlexanderFraltsov <kolonessens@yandex.ru>
2024-02-07 19:19:49 +0400

    feat: error handlind async implemented

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,20 +1,19 @@
-import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 3, b: 5, action: Action.Add, expected: 8 },
-    { a: 5, b: 3, action: Action.Subtract, expected: 2 },
-    { a: 5, b: 3, action: Action.Multiply, expected: 15 },
-    { a: 9, b: 3, action: Action.Divide, expected: 3 },
-    { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
-    { a: 2, b: 0, action: 'not action', expected: null },
-    { a: 'one', b: 'two', action: Action.Divide, expected: null },
+  { a: 3, b: 5, action: Action.Add, expected: 8 },
+  { a: 5, b: 3, action: Action.Subtract, expected: 2 },
+  { a: 5, b: 3, action: Action.Multiply, expected: 15 },
+  { a: 9, b: 3, action: Action.Divide, expected: 3 },
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  { a: 2, b: 0, action: 'not action', expected: null },
+  { a: 'one', b: 'two', action: Action.Divide, expected: null },
 ];
 
 describe('simpleCalculator', () => {
   test.each(testCases)(
-		`should return $expected for $a $action $b`,
-		({ a, b, action, expected }) =>
-			expect(simpleCalculator({ a, b, action }))
-				.toBe(expected),
-	);
+    `should return $expected for $a $action $b`,
+    ({ a, b, action, expected }) =>
+      expect(simpleCalculator({ a, b, action })).toBe(expected),
+  );
 });
